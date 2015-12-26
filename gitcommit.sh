cf398ed4d376545a9b414ab02d03ce44c109ecc0
krishna <krishnakalyan3@gmail.com>
2015-12-26 19:50:36 +0100

    Commit Date Sat Dec 26 19:50:35 CET 2015

--- gitcommit.sh
+++
@@ -1,6 +0,0 @@
-INPUTDATE=`date`
-git pull
-git add .
-git commit -m "Commit Date $INPUTDATE"
-git remote add origin_reg https://github.com/krishnakalyan3/Regression.git
-git push -u origin_reg master
